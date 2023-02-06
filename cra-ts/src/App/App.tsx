17997b160960f8ed2051f5b2dbc9648a7b2290d7
lessfish <lessfish@outlook.com>
2023-02-06 08:40:40 +0800

    feat: 完善 `cra-ts`

---
+++ cra-ts/src/App/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import React from 'react';
+import { Button } from '@arco-design/web-react'
+
+function App() {
+  return (
+    <div className="App-container">
+      <p>hello world</p>
+      <Button type='primary'>click</Button>
+    </div>
+  );
+}
+
+export default App;
--- cra-ts/src/index.tsx
+++ cra-ts/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import '@arco-design/web-react/dist/css/arco.css';
+import App from './App/App';
+// import '@arco-design/web-react/dist/css/arco.css';
+import '@arco-design/theme-ve-o-design/css/arco.css' // 源力主题
 
 ReactDOM.render(
   <React.StrictMode>
